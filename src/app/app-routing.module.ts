86a29558c728e9b784615a228b2d8a9dac41dc68
nipaaa <akternipa941@gmail.com>
2022-11-12 14:59:11 -0800

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutStudioComponent } from './about-studio/about-studio.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path:"", component: AppComponent},
+{path:"aboutStudio", component: AboutStudioComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
